1d9988c51bbcf5ca56312e3554b12e4376946f70
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-03 01:21:05 -0800

    Update main.cpp

--- C++/concurrent/main.cpp
+++ C++/concurrent/main.cpp
@@ -113,244 +113,244 @@ int main()
 
 
 
-//// task based currency
-//// task stealing
-//int main()
-//{
-//    cout<<"main thread ID " << this_thread::get_id()<<endl;
-//    vector<future<void>> futures;
-//    for(int i = 0; i < 10; i++)
-//    {
-//        auto ftr = async([](){
-//            this_thread::sleep_for(chrono::seconds(1));
-//            cout<<this_thread::get_id()<<" ";
-//        });
-//        futures.push_back(move(ftr));
-//    }
-//
-//    for_each(futures.begin(), futures.end(), [](future<void> & ftr)
-//    {
-//        ftr.wait();
-//    });
-//
-//    cout<<endl;
-//    this_thread::sleep_for(chrono::seconds(10));
-//
-//    return 0;
-//}
-//// main thread ID 0x107d405c0
-//// 0x700006231000 0x7000061ae000  0x700006337000 0x7000063ba000 0x7000062b4000 0x70000643d000 0x7000064c0000 0x7000065c6000 0x700006543000 0x700006649000
+// task based currency
+// task stealing
+int main()
+{
+   cout<<"main thread ID " << this_thread::get_id()<<endl;
+   vector<future<void>> futures;
+   for(int i = 0; i < 10; i++)
+   {
+       auto ftr = async([](){
+           this_thread::sleep_for(chrono::seconds(1));
+           cout<<this_thread::get_id()<<" ";
+       });
+       futures.push_back(move(ftr));
+   }
+
+   for_each(futures.begin(), futures.end(), [](future<void> & ftr)
+   {
+       ftr.wait();
+   });
+
+   cout<<endl;
+   this_thread::sleep_for(chrono::seconds(10));
+
+   return 0;
+}
+// main thread ID 0x107d405c0
+// 0x700006231000 0x7000061ae000  0x700006337000 0x7000063ba000 0x7000062b4000 0x70000643d000 0x7000064c0000 0x7000065c6000 0x700006543000 0x700006649000
 
 
-//// async
-//// simplify the promise and future
-//// simplify the exception
-//// Same as the normal function
-//string Fun()
-//{
-//    string str("string from future ");
-//    throw(exception());
-//    return str;
-//}
-//
-//int main()
-//{
-////    promise<string> prms;
-////    future<string> ftr = prms.get_future();
-////    thread th(&thFun, move(prms));
-//    auto ftr = async(&Fun);
-//    cout<<"str from main"<<endl;
-//    try
-//    {
-//        string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
-//        cout<<str<<endl;
-//    } catch (exception & e)
-//    {
-//        cout<<e.what()<<endl;
-//    }
-////    th.join();
-//    return 0;
-//}
-//// str from main
-//// std::exception
+// async
+// simplify the promise and future
+// simplify the exception
+// Same as the normal function
+string Fun()
+{
+   string str("string from future ");
+   throw(exception());
+   return str;
+}
 
-
-//// async
-//// simplify the promise and future
-//string Fun()
-//{
-//    string str("string from future ");
-//    return str;
-//}
-//
-//int main()
-//{
-////    promise<string> prms;
-////    future<string> ftr = prms.get_future();
-////    thread th(&thFun, move(prms));
-//    auto ftr = async(&Fun);
-//    cout<<"str from main"<<endl;
-//    string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
-//    cout<<str<<endl;
-////    th.join();
-//    return 0;
-//}
-//// str from main
-//// string from future
-
-
-//// promise and future
-//// exception
-//void thFun(promise<string> && prms)
-//{
-//    try
-//    {
-//        string str("string from future");
-//        throw(exception());
-//        prms.set_value(str);
-//    }catch(...)
-//    {
-//        prms.set_exception(current_exception());
-//    }
-//
-//}
-//
-//int main()
-//{
+int main()
+{
 //    promise<string> prms;
 //    future<string> ftr = prms.get_future();
 //    thread th(&thFun, move(prms));
-//    cout<<"str from main"<<endl;
-//    try
-//    {
-//        string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
-//        cout<<str<<endl;
-//    }catch (exception & e)
-//    {
-//        cout<<e.what()<<endl;
-//    }
+   auto ftr = async(&Fun);
+   cout<<"str from main"<<endl;
+   try
+   {
+       string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
+       cout<<str<<endl;
+   } catch (exception & e)
+   {
+       cout<<e.what()<<endl;
+   }
 //    th.join();
-//    return 0;
-//}
+   return 0;
+}
 // str from main
 // std::exception
 
 
+// async
+// simplify the promise and future
+string Fun()
+{
+   string str("string from future ");
+   return str;
+}
 
-//// promise and future
-//// promise pass by reference
-//void thFun(promise<string> & prms)
-//{
-//    string str("string from future");
-//    cout<<"promise str address:"<<(void*)str.data()<<endl;
-//    prms.set_value(str);
-//}
-//
-//int main()
-//{
-//    promise<string> prms;
-//    thread th(&thFun, ref(prms));
-//    cout<<"string from main "<<endl;
-//    future<string> ftr = prms.get_future();
-//    string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
-//    cout<<str<<endl;
-//    cout<<"main str address:"<<(void*)str.data()<<endl;
-//    th.join();
-//    return 0;
-//}
-//// string from main
-//// promise str address:0x700006e23bb9
-//// string from future
-//// main str address:0x7ffeed988831
-
-
-
-//// promise and future
-//// promise pass by move
-//void thFun(promise<string> && prms)
-//{
-//    string str("string from future");
-//    prms.set_value(str);
-//}
-//
-//int main()
-//{
+int main()
+{
 //    promise<string> prms;
 //    future<string> ftr = prms.get_future();
 //    thread th(&thFun, move(prms));
-//    cout<<"str from main"<<endl;
-//    string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
-//    cout<<str<<endl;
+   auto ftr = async(&Fun);
+   cout<<"str from main"<<endl;
+   string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
+   cout<<str<<endl;
 //    th.join();
-//    return 0;
-//}
-//// str from main
-//// string from future
+   return 0;
+}
+// str from main
+// string from future
 
 
-//// Dangling reference
-//void thFun(int &i)
-//{
-//    cout<<"thread "<<i<<endl;
-//}
-//
-//void test(vector<thread>&  workers)
-//{
-//    for(int i = 0; i < 8; i++)
-//    {
-//        auto th = thread(&thFun, ref(i));
-//        /*
-//         * thread 5thread
-//         * thread 6Hello, World!
-//         *
-//         * thread 71536
-//         * thread thread 1536
-//         * thread 1536
-//         *
-//         * thread 0
-//         * 1536
-//         * 传reference 到thread很危险
-//         * 每一个thread有一个自己的 stack, 传引用会复制一个指针到该thread的stack上
-//         */
-//
-//        workers.push_back(move(th));
-//        assert(!th.joinable());
-//    }
-//}
-//
-//int main() {
-//    vector<thread> workers;
-//
-//    test(workers);
-//
-//    std::cout << "Hello, World!" << std::endl;
-//    for_each(workers.begin(), workers.end(), [](thread& th){
-//        assert(th.joinable());
-//        th.join();
-//    });
-//
-//    return 0;
-//}
+// promise and future
+// exception
+void thFun(promise<string> && prms)
+{
+   try
+   {
+       string str("string from future");
+       throw(exception());
+       prms.set_value(str);
+   }catch(...)
+   {
+       prms.set_exception(current_exception());
+   }
+
+}
+
+int main()
+{
+   promise<string> prms;
+   future<string> ftr = prms.get_future();
+   thread th(&thFun, move(prms));
+   cout<<"str from main"<<endl;
+   try
+   {
+       string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
+       cout<<str<<endl;
+   }catch (exception & e)
+   {
+       cout<<e.what()<<endl;
+   }
+   th.join();
+   return 0;
+}
+str from main
+std::exception
 
 
-//// lamda version
-//int main() {
-//    vector<thread> workers;
-//    for(int i = 0; i < 8; i++)
-//    {
-//        auto th = thread([i](){
-//            cout<<"thread "<<i<<endl;
-//        });
-//
-//        workers.push_back(move(th));
-//        assert(!th.joinable());
-//    }
-//
-//    std::cout << "Hello, World!" << std::endl;
-//    for_each(workers.begin(), workers.end(), [](thread& th){
-//        assert(th.joinable());
-//        th.join();
-//    });
-//
-//    return 0;
-//}
+
+// promise and future
+// promise pass by reference
+void thFun(promise<string> & prms)
+{
+   string str("string from future");
+   cout<<"promise str address:"<<(void*)str.data()<<endl;
+   prms.set_value(str);
+}
+
+int main()
+{
+   promise<string> prms;
+   thread th(&thFun, ref(prms));
+   cout<<"string from main "<<endl;
+   future<string> ftr = prms.get_future();
+   string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
+   cout<<str<<endl;
+   cout<<"main str address:"<<(void*)str.data()<<endl;
+   th.join();
+   return 0;
+}
+// string from main
+// promise str address:0x700006e23bb9
+// string from future
+// main str address:0x7ffeed988831
+
+
+
+// promise and future
+// promise pass by move
+void thFun(promise<string> && prms)
+{
+   string str("string from future");
+   prms.set_value(str);
+}
+
+int main()
+{
+   promise<string> prms;
+   future<string> ftr = prms.get_future();
+   thread th(&thFun, move(prms));
+   cout<<"str from main"<<endl;
+   string str = ftr.get(); // 如果还没有 prms.set_value(str)， main将会阻塞在这里
+   cout<<str<<endl;
+   th.join();
+   return 0;
+}
+// str from main
+// string from future
+
+
+// Dangling reference
+void thFun(int &i)
+{
+   cout<<"thread "<<i<<endl;
+}
+
+void test(vector<thread>&  workers)
+{
+   for(int i = 0; i < 8; i++)
+   {
+       auto th = thread(&thFun, ref(i));
+       /*
+        * thread 5thread
+        * thread 6Hello, World!
+        *
+        * thread 71536
+        * thread thread 1536
+        * thread 1536
+        *
+        * thread 0
+        * 1536
+        * 传reference 到thread很危险
+        * 每一个thread有一个自己的 stack, 传引用会复制一个指针到该thread的stack上
+        */
+
+       workers.push_back(move(th));
+       assert(!th.joinable());
+   }
+}
+
+int main() {
+   vector<thread> workers;
+
+   test(workers);
+
+   std::cout << "Hello, World!" << std::endl;
+   for_each(workers.begin(), workers.end(), [](thread& th){
+       assert(th.joinable());
+       th.join();
+   });
+
+   return 0;
+}
+
+
+// lamda version
+int main() {
+   vector<thread> workers;
+   for(int i = 0; i < 8; i++)
+   {
+       auto th = thread([i](){
+           cout<<"thread "<<i<<endl;
+       });
+
+       workers.push_back(move(th));
+       assert(!th.joinable());
+   }
+
+   std::cout << "Hello, World!" << std::endl;
+   for_each(workers.begin(), workers.end(), [](thread& th){
+       assert(th.joinable());
+       th.join();
+   });
+
+   return 0;
+}
