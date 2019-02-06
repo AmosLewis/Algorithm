1bef4efe9aa1617f6a1de6521722f02a0091e59b
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-06 08:51:04 -0800

    Update Read_N_Characters_Given_Read4.cpp

--- C++/practice/src/Read_N_Characters_Given_Read4.cpp
+++ C++/practice/src/Read_N_Characters_Given_Read4.cpp
@@ -29,3 +29,31 @@ public:
         return min(offset, n);
     }
 };
+
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        // 递归
+        int current = read4(buf);
+        // 要的不多
+        // current = 3, n = 2 有3个字符取只要俩
+        // current = 4, n = 4 有四个字符，只要4个
+        // current = 4, n = 2 有四个字符，只要2个
+        if(current >= n) return n;
+        // 要的多,有的少
+        // current = 2, n = 4
+        // current = 2, n = 10
+        if(current < 4) return current;
+        // 递归, buf 始终指向最后一位
+        return 4 + read(&buf[4], n - 4);
+    }
+};
