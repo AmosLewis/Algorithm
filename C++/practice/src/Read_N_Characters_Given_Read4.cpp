cac96f4a74378f2fa19a83c076cb9e9cc5a117e2
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-06 08:42:09 -0800

    Create Read_N_Characters_Given_Read4.cpp

---
+++ C++/practice/src/Read_N_Characters_Given_Read4.cpp
@@ -0,0 +1,31 @@
+/*
+http://www.cnblogs.com/grandyang/p/5174322.html
+*/
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        // 用一个文件指针offset指向buf的结尾， 指针的大小就是文件的大小
+        // 初始时候指针长度为0
+        int offset = 0;
+        while(true)
+        {
+            // 获取当前读到的字符长度， 字符存入buf的结尾
+            int current = read4(buf + offset);
+            // 没有新的输入，说明读完了，直接跳出循环
+            if(current == 0) break;
+            // 文件指针向后移动
+            offset += current;
+        }
+        // 如果offset = 3 , n = 100, 只有3个字符，却要求读100个，则只返回3
+        return min(offset, n);
+    }
+};
