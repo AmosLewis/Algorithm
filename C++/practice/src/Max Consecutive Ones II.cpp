4444e4913ad23055963f0b1040c1b0b1b6b6b8b4
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-08 20:28:47 -0800

    Update Max Consecutive Ones II.cpp

--- C++/practice/src/Max Consecutive Ones II.cpp
+++ C++/practice/src/Max Consecutive Ones II.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) { 
+        // 最优解
         // 法二 同向双指针，维护一个sliding windows [left, right]
         int res = 0;
         int left = 0;
@@ -24,3 +25,32 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) { 
+        // 法三 同向双指针，维护一个sliding windows [left, right]
+        // 不过这次不能用数组下表访问left
+        // 这次用一个Q记录下所有的0的位置， 用right记录，然后最先push进的的就是left
+        // 移动left 后要pop
+        int res = 0;
+        int left = 0;
+        int zeros = 0;
+        int k = 1;
+        queue<int> Q;
+        for(int right = 0; right < nums.size(); right++)
+        {
+            if(nums[right] == 0) Q.push(right);
+            while(Q.size() > k)
+            {
+                // 移动left 
+                left = Q.front() + 1;
+                Q.pop();
+            }
+            
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
+
