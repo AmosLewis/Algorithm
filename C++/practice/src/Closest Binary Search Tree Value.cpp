794adf4e64659d035439956fc0a3451a8ce3d1f7
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-13 11:13:53 -0800

    Update Closest Binary Search Tree Value.cpp

--- C++/practice/src/Closest Binary Search Tree Value.cpp
+++ C++/practice/src/Closest Binary Search Tree Value.cpp
@@ -1,4 +1,4 @@
-
+http://www.cnblogs.com/grandyang/p/5237170.html
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -23,5 +23,31 @@ public:
             root = root->val > target ? root->left : root->right;
         }
         return res;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        // 法2 递归的解法
+        int a = root->val;
+        TreeNode* t = target < a ? root->left : root->right;
+        
+        // 递归的返回条件，到叶子节点
+        if(!t) return a;
+        // 递归，找出子树中的最近的值
+        int b = closestValue(t, target);
+        // 比较当前node与其子node关系返回点，谁更小就返回谁
+        return abs(a-target) < abs(b-target) ? a : b; 
     }
 };
