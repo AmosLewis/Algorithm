17f5ec5b3474b02193bf5763c562e2f6053327ff
Chi_Liu <chil1@alumni.cmu.edu>
2019-02-25 08:11:31 -0800

    Update Kth smallest in 2D sorted Array.cpp

--- C++/practice/src/Kth smallest in 2D sorted Array.cpp
+++ C++/practice/src/Kth smallest in 2D sorted Array.cpp
@@ -1,4 +1,5 @@
-// kth smallest in 2d sorted Matrix
+// 											BFS2,Dijkstra思想
+// 								kth smallest in 2d sorted Matrix
 struct Node
 {
 	int val;
@@ -14,6 +15,7 @@ struct compare
 }
 int kthSmallest2D(vector<vector<int>>& matrix, int k)
 {
+	// expand k次， generate 2k 次
 	if( k < 0 || matrix.size() == 0 || matrix[0].size()==0)
 	{
 		return INT_MAX;
@@ -24,6 +26,7 @@ int kthSmallest2D(vector<vector<int>>& matrix, int k)
 	// step1: 建立一个最小堆,
 	priority_queue<Node, vector<Node>, compare> min_heap;
 	// 记录某一个元素是否被访问过
+	// 20 与 15 都会访问25
 	vector<vector<bool>> visited(row,vector<int>(col, false));
 	// 初始化最小堆
 	min_heap.push({matrix[0][0], 0, 0});
@@ -36,8 +39,9 @@ int kthSmallest2D(vector<vector<int>>& matrix, int k)
 		Node cur = min_heap.top();
 		int r = cur.row;
 		int c = cur.col;
-		min_heap.pop();
+		min_heap.pop();// logn
 
+		// insert two neibourhood: 2log(k)
 		// 插入堆顶的下方点
 		if(r + 1 < row - 1 && visited[r + 1][c] == false)
 		{
@@ -50,12 +54,17 @@ int kthSmallest2D(vector<vector<int>>& matrix, int k)
 			min_heap.push({matrix[r][c + 1], r, c + 1});
 			visited[r][c + 1] = true;
 		}
-		result = cur.val;
+		result = cur.val; // 或者这里弹出第K次的时候，得到的就是结果
 	}
 
 	// 堆顶即为第K小
 	return min_heap.top();
 }
+// Time: there are k element, 
+//		for each element, we need to pop top element for expansion, logk
+//		for expansion, push 2 neighbours to heap, 2logk
+// total is time complexity is O(3klogk) = O(klogk)
+// space O(2k) = O(k)
 
 // 10, 20, 30, 40
 // 15, 25, 35, 45
